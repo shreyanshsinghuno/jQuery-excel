72f8592220d7f9a741027cb9dfb775cb31ce7380
shreyanshsinghuno <shreyanshsinghuno@gmail.com>
2021-06-18 01:04:17 +0100

    script changed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-  // jQuery methods go here...
   let cellContainer = $(".input-cell-container");
 
   for (let i = 1; i <= 100; i++) {
-    let ans = " ";
+    let ans = "";
+
     let n = i;
+
     while (n > 0) {
       let rem = n % 26;
       if (rem == 0) {
@@ -15,22 +16,21 @@ $(document).ready(function () {
         n = Math.floor(n / 26);
       }
     }
-    let column = $(
-      `<div class = "column-name colId-${i}" id = "colCod-${ans}  "> ${ans} </div>`
-    );
 
-    let row = $(`<div class="row-name" , id = "rowid-${i}">${i}</div>`);
-    $(".row-name-container").append(row);
+    let column = $(
+      `<div class="column-name colId-${i}" id="colCod-${ans}">${ans}</div>`
+    );
     $(".column-name-container").append(column);
+    let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+    $(".row-name-container").append(row);
   }
 
   for (let i = 1; i <= 100; i++) {
-    let row = $(`<div class = "cell-row"></div>`);
+    let row = $(`<div class="cell-row"></div>`);
     for (let j = 1; j <= 100; j++) {
-      let colCode = $(`.colId-${j}`).attr("id");
-      let code = colCode.slice(1);
+      let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
       let column = $(
-        `<div class = "input-cell" contenteditable="true" id = "row-${i}-column-${j}" data = "${code}"></div>`
+        `<div class="input-cell" contenteditable="false" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`
       );
       row.append(column);
     }
@@ -46,8 +46,102 @@ $(document).ready(function () {
     $(this).toggleClass("selected");
   });
 
-  $(".input-cell").click(function () {
+  $(".input-cell").click(function (e) {
+    if (e.ctrlKey) {
+      let [rowId, colId] = getRowCol(this);
+      if (rowId > 1) {
+        let topCellSelected = $(`#row-${rowId - 1}-col-${colId}`).hasClass(
+          "selected"
+        );
+        if (topCellSelected) {
+          $(this).addClass("top-cell-selected");
+          $(`#row-${rowId - 1}-col-${colId}`).addClass("bottom-cell-selected");
+        }
+      }
+      if (rowId < 100) {
+        let bottomCellSelected = $(`#row-${rowId + 1}-col-${colId}`).hasClass(
+          "selected"
+        );
+        if (bottomCellSelected) {
+          $(this).addClass("bottom-cell-selected");
+          $(`#row-${rowId + 1}-col-${colId}`).addClass("top-cell-selected");
+        }
+      }
+      if (colId > 1) {
+        let leftCellSelected = $(`#row-${rowId}-col-${colId - 1}`).hasClass(
+          "selected"
+        );
+        if (leftCellSelected) {
+          $(this).addClass("left-cell-selected");
+          $(`#row-${rowId}-col-${colId - 1}`).addClass("right-cell-selected");
+        }
+      }
+      if (colId < 100) {
+        let rightCellSelected = $(`#row-${rowId}-col-${colId + 1}`).hasClass(
+          "selected"
+        );
+        if (rightCellSelected) {
+          $(this).addClass("right-cell-selected");
+          $(`#row-${rowId}-col-${colId + 1}`).addClass("left-cell-selected");
+        }
+      }
+      $(this).addClass("selected");
+    } else {
+      $(".input-cell.selected").removeClass("selected");
+      $(this).addClass("selected");
+    }
+  });
+
+  $(".input-cell").dblclick(function () {
     $(".input-cell.selected").removeClass("selected");
     $(this).addClass("selected");
+    $(this).attr("contenteditable", "true");
+    $(this).focus();
   });
+
+  $(".input-cell").blur(function () {
+    $(".input-cell.selected").attr("contenteditable", "false");
+  });
+
+  $(".input-cell-container").scroll(function () {
+    $(".column-name-container").scrollLeft(this.scrollLeft);
+    $(".row-name-container").scrollTop(this.scrollTop);
+  });
+});
+
+function getRowCol(ele) {
+  let idArray = $(ele).attr("id").split("-");
+  let rowId = parseInt(idArray[1]);
+  let colId = parseInt(idArray[3]);
+  return [rowId, colId];
+}
+
+function updateCell(property, value) {
+  $(".input-cell.selected").each(function () {
+    $(this).css(property, value);
+  });
+}
+
+$(".icon-bold").click(function () {
+  if ($(this).hasClass("selected")) {
+    updateCell("font-weight", "");
+  } else {
+    updateCell("font-weight", "bold");
+  }
+});
+
+$(".icon-italic").click(function () {
+  if ($(this).hasClass("selected")) {
+    updateCell("font-style", "");
+  } else {
+    updateCell("font-style", "italic");
+  }
+});
+
+$(".icon-underline").click(function () {
+  if ($(this).hasClass("selected")) {
+    updateCell("text-decoration", "");
+  } else {
+    updateCell("text-decoration", "underline");
+  }
 });
